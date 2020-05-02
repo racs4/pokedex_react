0cd6606b76814807e0f0b57a6dde81b05715383d
Rheidner <rheidner.achiles@gmail.com>
2020-05-02 18:34:16 -0300

    style: adiciona o corpo do pokedex e seus estilos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 //COMPONENTS
-import Header from './components//header/header.js';
+import Header from './components/header/header.js';
+import Body from './components/body/body.js';
 
 const App = () => {
     return (
         <div className="main">
             < Header />
+            < Body />
         </div>
     );
 }
---
+++ src/components/body/body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './body.css';
+
+const Body = () => {
+    return (
+        <div className="body">
+            Corpo
+        </div>
+    );
+}
+
+export default Body;
