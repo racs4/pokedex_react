fdf45f3f8dc25e72f7206c740b6e722fd5830329
Rheidner <rheidner.achiles@gmail.com>
2020-05-02 18:49:53 -0300

    style: Adiciona a parte esquerda e direita do corpo da pokedex
    Cria os dois componentes filhos do corpo
    Tambem adiciona seus estilos

---
+++ src/components/body/body_left/body_left.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './body_left.css';
+
+const BodyLeft = () => {
+    return (
+        <div className="body__left">
+            Left
+        </div>
+    );
+}
+
+export default BodyLeft;
---
+++ src/components/body/body_right/body_right.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './body_right.css';
+
+const BodyRight = () => {
+    return (
+        <div className="body__right">
+            Right
+        </div>
+    );
+}
+
+export default BodyRight;
