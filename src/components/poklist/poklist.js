0f1c687715e5ebbbf8eb021fb8d6b28fd491bf02
Rheidner <rheidner.achiles@gmail.com>
2020-05-02 20:33:07 -0300

    feat: Adiciona componente poklist
    O componente sera responsavel por organizar a lista dos pokemons

---
+++ src/components/poklist/poklist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './poklist.css';
+
+const PokList = () => {
+    return (
+        <div className="poklist">
+            Rights
+        </div>
+    );
+}
+
+export default PokList;
--- src/layout/body/body_right/body_right.js
+++ src/layout/body/body_right/body_right.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './body_right.css';
+import PokList from '../../../components/poklist/poklist';
 
 const BodyRight = () => {
     return (
         <div className="body__right">
-            Right
+            <PokList />
         </div>
     );
 }
