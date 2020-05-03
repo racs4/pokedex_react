190a5b55743196ab6458f303a92ddd46c090f757
Rheidner <rheidner.achiles@gmail.com>
2020-05-02 21:22:22 -0300

    style: Adiciona estilo webkit a scrollbar
    Adiciona estilo a scrollbar da pagina

--- src/components/poklist/poklist.js
+++ src/components/poklist/poklist.js
@@ -3,11 +3,20 @@ import './poklist.css';
 import PokItem from '../pokitem/pokitem';
 
 const PokList = () => {
+
+    let itens = [];
+
+    for (let i = 0; i < 12; i++) {
+        itens.push(
+            (<div key={i} className="poklist__pokitem" >
+                <PokItem />
+            </div>)
+        );
+    }
+
     return (
         <div className="poklist">
-            <div className="poklist__pokitem" >
-                <PokItem />
-            </div>
+            {itens}
         </div>
     );
 }
